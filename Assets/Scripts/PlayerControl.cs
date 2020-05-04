8d9d36521cd2770dfdad112f09499cda767558ec
Kevin Grandemange <grandemange.kevin@gmail.com>
2020-05-04 08:41:12 +0200

    Testing tilemap

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 4.5f;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(Time.deltaTime * speed * direction);
+    }
+}

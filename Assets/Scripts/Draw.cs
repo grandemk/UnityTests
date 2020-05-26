8ab69e435bf31f9bc325312a12b20e1dddd213f4
Kevin Grandemange <grandemange.kevin@gmail.com>
2020-05-26 19:11:26 +0200

    Make the evolution automatic instead of pumping the space key

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -12,6 +12,12 @@ public class Draw : MonoBehaviour
 
     Color trunkColor = ToColor("794A1C");
     Color leavesColor = ToColor("133609");
+
+    float leavesThreshold = 0.3f;
+    int numIteration = 0;
+    bool paused = false;
+    int maxIteration = 10;
+
     LineRenderer CreateLine(Vector3 a, Vector3 b, Color color)
     {
         var obj = new GameObject("Line");
@@ -62,44 +68,60 @@ public class Draw : MonoBehaviour
         return new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1f);
     }
 
+    void Update()
+    {
+      if(Input.GetKeyDown("space"))
+        paused = !paused;
+      if(Input.GetKeyDown("m"))
+        maxIteration += 1;
+    }
+
+    IEnumerator GrowTree()
+    {
+        while(true)
+        {
+            while (paused || numIteration > maxIteration)
+                yield return null;
+
+            numIteration += 1;
+            List<GenTree> toAdd = new List<GenTree>();
+            List<GenTree> toRemove = new List<GenTree>();
+            for (int i = curLeaves.Count - 1; i >= 0; --i)
+            {
+                GenTree curTree = curLeaves[i];
+                int numChild = 2;
+                for (int j = 0; j < numChild; ++j)
+                {
+                    float r = curTree.r * Random.Range(0.5f, 0.9f);
+                    var intersection = Random.Range(0.2f, 0.9f);
+                    float theta = curTree.theta + Random.Range(-0.30f, 0.30f) * Mathf.PI;
+
+                    Color color = trunkColor;
+                    if (r < leavesThreshold)
+                        color = leavesColor;
+
+                    Vector3 a = Vector3.Lerp(curTree.line.GetPosition(0), curTree.line.GetPosition(1), intersection);
+                    var b = a + CreatePolarVector3(r, theta);
+                    curTree.children.Add(new GenTree(CreateLine(a, b, color), r, theta));
+                }
+                toAdd.AddRange(curTree.children);
+                curLeaves.RemoveAt(i);
+            }
+            curLeaves.AddRange(toAdd);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
     void Start()
     {
         trunkColor = RandomColor();
         leavesColor = RandomColor();
+        leavesThreshold = Random.Range(0.3f, 0.6f);
 
         var r = Random.Range(1f, 5f);
         var theta = Random.Range(0.30f, 0.70f) * Mathf.PI;
         curLeaves.Add(new GenTree(CreateLine(Vector3.zero, CreatePolarVector3(r, theta), trunkColor), r, theta));
-    }
 
-    void Update()
-    {
-        if(!Input.GetKeyDown("space"))
-          return;
-        List<GenTree> toAdd = new List<GenTree>();
-        List<GenTree> toRemove = new List<GenTree>();
-        for(int i = curLeaves.Count - 1; i >= 0; --i)
-        {
-            GenTree curTree = curLeaves[i];
-            int numChild = 2;
-            for (int j = 0; j < numChild; ++j)
-            {
-                float r = curTree.r * Random.Range(0.5f, 0.9f);
-                var intersection = Random.Range(0.2f, 0.9f);
-                float theta = curTree.theta + Random.Range(-0.30f, 0.30f) * Mathf.PI;
-                float leavesThreshold = 0.3f;
-
-                Color color = trunkColor;
-                if(r < leavesThreshold)
-                  color = leavesColor;
-
-                Vector3 a = Vector3.Lerp(curTree.line.GetPosition(0), curTree.line.GetPosition(1), intersection);
-                var b = a + CreatePolarVector3(r, theta);
-                curTree.children.Add(new GenTree(CreateLine(a, b, color), r, theta));
-            }
-            toAdd.AddRange(curTree.children);
-            curLeaves.RemoveAt(i);
-        }
-        curLeaves.AddRange(toAdd);
+        StartCoroutine("GrowTree");
     }
 }
